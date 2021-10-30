cc8d8889af3c6f51635a318508da345a044cf8ff
Mariana_C_Ruiz <mchierandaruiz0@gmail.com>
2021-10-30 03:20:20 -0300

    change

---
+++ lib/counter.rb
@@ -0,0 +1,10 @@
+class Counter
+  @qtd = 0
+
+  def self.qtd
+    @qtd
+  end
+  def self.increment
+    @qtd +=1
+  end
+end
---
+++ spec/matchers/classes/attributes_let_spec .rb
@@ -0,0 +1,13 @@
+require 'person'
+
+describe 'Class Attributes' do
+  before(:each) do
+    @person = Person.new
+  end
+  it 'have_attributes' do
+    @person.name = 'Fulano'
+    @person.age = 12
+    expect(@person).to have_attributes(name: starting_with('F'), age: (be >= 10))
+    expect(@person).to have_attributes(name: 'Fulano', age: 12)
+  end
+end
